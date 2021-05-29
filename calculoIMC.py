43159c0b0e0d8de9adaf616ee5434d6e222d3dac
André Camargo <andrefelipe_camargo@hotmail.com>
2021-05-29 19:20:40 -0300

    Update calculoIMC.py

--- calculoIMC.py
+++ calculoIMC.py
@@ -1,21 +1,23 @@
-print('\033[1;97m-' * 20)
+print('-' * 20)
 peso = float(input('Qual seu peso? '))
 altura = float(input('Qual sua altura? '))
-print('\033[1;97m-' * 20)
+print('-' * 20)
 
 imc = peso / (altura * altura)
 
-if imc < 18.5:
-    print('\033[1;91mAbaixo do peso.\033[1;91m\n')
+print('O IMC dessa pessoa é de {:.1f}.'.format(imc))
 
-elif imc > 18.5 and imc <= 25:
-    print('\033[1;32mPeso ideal\033[m\n')
+if imc < 18.5:
+    print('Abaixo do peso.')
+
+elif imc >= 18.5 and imc <= 25:
+    print('Peso ideal')
 
 elif imc >= 25 and imc <= 30:
-    print('\033[1;33mSobrepeso\033[1;33m\n')
+    print('Sobrepeso')
 
 elif imc >= 30 and imc <= 40:
-    print('\033[1;31mObesidade\033[1;31m\n')
+    print('Obesidade')
 
 elif imc > 40:
-    print('\033[1;91mObesidade mórbida\033[1;91m\n')
+    print('Obesidade mórbida')
